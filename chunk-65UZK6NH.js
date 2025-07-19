4d356472abe0aa3b02d74f0865ebf7bd2930b427
ac484 <ac484@users.noreply.github.com>
2025-07-19 16:14:19 +0000

    Automated deployment: Sat Jul 19 16:14:19 UTC 2025 9c0eed8923f7a5609b357720cc5705b45869cad4

---
+++ chunk-65UZK6NH.js
@@ -0,0 +1,1 @@
+import{V as o}from"./chunk-HHES5VLB.js";import{Oa as i,da as n}from"./chunk-A76PPZD6.js";var y=(()=>{class e{srv=n(o);transform(r,t){return this.srv.format(r,t)}static \u0275fac=function(t){return new(t||e)};static \u0275pipe=i({name:"price",type:e,pure:!0})}return e})();export{y as a};
