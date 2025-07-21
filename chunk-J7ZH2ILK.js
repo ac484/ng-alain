2cde5b5a381615b615fcf422211b59dd5e55afdd
ac484 <ac484@users.noreply.github.com>
2025-07-21 06:27:15 +0000

    Automated deployment: Mon Jul 21 06:27:15 UTC 2025 e2bfd8b1c9faafa4dfbd15ed80fc773f3685e8f6

---
+++ chunk-J7ZH2ILK.js
@@ -0,0 +1,1 @@
+import{c as o}from"./chunk-UMGDNG4Q.js";import{nb as i,oa as n}from"./chunk-67N3UCKF.js";var y=(()=>{class e{srv=n(o);transform(r,t){return this.srv.format(r,t)}static \u0275fac=function(t){return new(t||e)};static \u0275pipe=i({name:"price",type:e,pure:!0})}return e})();export{y as a};
