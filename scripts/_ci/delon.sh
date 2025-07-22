f92fba004f4aebefcf163a4365cc05427a2ace42
𓂺 <ac7x7x@gmail.com>
2025-07-22 21:18:19 +0800

    更新 delon.sh 腳本中的 GitHub 倉庫來源，將 @delon 相關套件的下載地址更改為新的倉庫，以確保獲取最新版本的 delon-builds。

--- scripts/_ci/delon.sh
+++ scripts/_ci/delon.sh
@@ -8,7 +8,7 @@ cd $(dirname $0)/../..
 
 echo "Download latest @delon version"
 rm -rf delon-builds
-git clone --depth 1 https://github.com/ng-alain/delon-builds.git
+git clone --depth 1 https://github.com/ac484/delon-builds.git
 rm -rf node_modules/@delon
 # rm -rf node_modules/ng-alain
 rsync -am delon-builds/ node_modules/
