d42927ed225d1393d3681857d9f1ad5ef2beea51
ac484 <ac484@users.noreply.github.com>
2025-07-23 17:52:35 +0000

    Automated deployment: Wed Jul 23 17:52:35 UTC 2025 7e3cfd3e4b5e7e720c60a62663171f8b26bebe92

---
+++ chunk-QPOGXE24.js
@@ -0,0 +1,1 @@
+import"./chunk-57BLH6QZ.js";var o=[{path:"",loadComponent:()=>import("./chunk-KBQ4F642.js").then(t=>t.DeptListComponent),data:{title:"\u90E8\u9580\u5217\u8868"}}];export{o as routes};
