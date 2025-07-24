7d154174db322779d7b29482fe05443aa16258de
ac484 <ac484@users.noreply.github.com>
2025-07-24 16:45:38 +0000

    Automated deployment: Thu Jul 24 16:45:38 UTC 2025 da7529fdde72c64740f5a2df00f5078e2d4888b8

--- chunk-GPG7RRUN.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./chunk-LSDOUZNF.js";import{Ib as i,ta as n}from"./chunk-IDL4MXQM.js";var y=(()=>{class e{srv=n(o);transform(r,t){return this.srv.format(r,t)}static \u0275fac=function(t){return new(t||e)};static \u0275pipe=i({name:"price",type:e,pure:!0})}return e})();export{y as a};
