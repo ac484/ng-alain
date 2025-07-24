73342c04e03bbcc1358ccb8c18d272949c1021b5
ac484 <ac484@users.noreply.github.com>
2025-07-24 09:18:05 +0000

    Automated deployment: Thu Jul 24 09:18:05 UTC 2025 6b9d05fd24b6fd76a8a64ae2a96dffa4049f04a6

---
+++ chunk-IR2BJHVA.js
@@ -0,0 +1,1 @@
+import"./chunk-57BLH6QZ.js";var o=[{path:"",loadComponent:()=>import("./chunk-JKVOORGG.js").then(t=>t.AccountListComponent),data:{title:"\u5E33\u865F\u5217\u8868"}}];export{o as routes};
