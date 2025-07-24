88cc9d8ae8f7e82c80bc909f8ec508117f16a668
ac484 <ac7x7x@gmail.com>
2025-07-25 00:51:11 +0800

    t2

--- src/app/routes/hub/contract/contract-list.component.ts
+++ src/app/routes/hub/contract/contract-list.component.ts
@@ -3,20 +3,22 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { ContractService } from './contract.service';
 import { Contract } from './contract.model';
-import { NzModalService, NzModalRef } from 'ng-zorro-antd/modal';
 import { FabComponent } from '../basic/widget/fab.component';
 import { HubCrudService } from '../fire-crud/hub-crud.service';
 
 @Component({
   selector: 'contract-list',
   standalone: true,
-  imports: [CommonModule, FormsModule, NzTableModule, NzButtonModule, NzSelectModule, FabComponent],
+  imports: [CommonModule, FormsModule, NzTableModule, NzButtonModule, NzInputModule, NzPopconfirmModule, FabComponent],
   template: `
-    <app-fab (onAction)="add()"></app-fab>
-    <nz-table [nzData]="contracts" nzBordered>
+    <app-fab (onAction)="addRow()"></app-fab>
+    <br />
+    <br />
+    <nz-table #editRowTable nzBordered [nzData]="contracts">
       <thead>
         <tr>
           <th>序號</th>
@@ -29,103 +31,84 @@ import { HubCrudService } from '../fire-crud/hub-crud.service';
         </tr>
       </thead>
       <tbody>
-        <tr *ngFor="let c of contracts">
-          <td>{{ c.contractSerial }}</td>
-          <td>
-            <div *ngIf="!(editId === c.key && editField === 'client')" (click)="startEdit(c.key, 'client')">
-              {{ c.client }}
-            </div>
-            <nz-select
-              *ngIf="editId === c.key && editField === 'client'"
-              [(ngModel)]="editValue"
-              (ngModelChange)="stopEdit(c, 'client')"
-              nzSize="small"
-              [nzOpen]="true"
-              style="min-width: 100px"
-            >
-              <nz-option *ngFor="let cl of clients" [nzValue]="cl" [nzLabel]="cl"></nz-option>
-            </nz-select>
-          </td>
-          <td>
-            <div *ngIf="!(editId === c.key && editField === 'contractName')" (click)="startEdit(c.key, 'contractName')">
-              {{ c.contractName }}
-            </div>
-            <input
-              *ngIf="editId === c.key && editField === 'contractName'"
-              type="text"
-              nz-input
-              [(ngModel)]="editValue"
-              (blur)="stopEdit(c, 'contractName')"
-            />
-          </td>
-          <td>
-            <div *ngIf="!(editId === c.key && editField === 'contractCode')" (click)="startEdit(c.key, 'contractCode')">
-              {{ c.contractCode }}
-            </div>
-            <input
-              *ngIf="editId === c.key && editField === 'contractCode'"
-              type="text"
-              nz-input
-              [(ngModel)]="editValue"
-              (blur)="stopEdit(c, 'contractCode')"
-            />
-          </td>
-          <td>
-            <div *ngIf="!(editId === c.key && editField === 'feeCode')" (click)="startEdit(c.key, 'feeCode')">
-              {{ c.feeCode }}
-            </div>
-            <input
-              *ngIf="editId === c.key && editField === 'feeCode'"
-              type="text"
-              nz-input
-              [(ngModel)]="editValue"
-              (blur)="stopEdit(c, 'feeCode')"
-            />
-          </td>
-          <td>
-            <div *ngIf="!(editId === c.key && editField === 'amount')" (click)="startEdit(c.key, 'amount')">
-              {{ c.amount }}
-            </div>
-            <input
-              *ngIf="editId === c.key && editField === 'amount'"
-              type="number"
-              nz-input
-              [(ngModel)]="editValue"
-              (blur)="stopEdit(c, 'amount')"
-            />
-          </td>
-          <td>
-            <button nz-button nzType="link" (click)="remove(c)">刪除</button>
-          </td>
-        </tr>
+        @for (c of editRowTable.data; track c) {
+          <tr class="editable-row">
+            <td>{{ c.contractSerial }}</td>
+            <td>
+              <div class="editable-cell" [hidden]="editId === c.key" (click)="startEdit(c.key)">
+                {{ c.client }}
+              </div>
+              <input [hidden]="editId !== c.key" type="text" nz-input [(ngModel)]="c.client" (blur)="stopEdit(c)" />
+            </td>
+            <td>
+              <div class="editable-cell" [hidden]="editId === c.key" (click)="startEdit(c.key)">
+                {{ c.contractName }}
+              </div>
+              <input [hidden]="editId !== c.key" type="text" nz-input [(ngModel)]="c.contractName" (blur)="stopEdit(c)" />
+            </td>
+            <td>
+              <div class="editable-cell" [hidden]="editId === c.key" (click)="startEdit(c.key)">
+                {{ c.contractCode }}
+              </div>
+              <input [hidden]="editId !== c.key" type="text" nz-input [(ngModel)]="c.contractCode" (blur)="stopEdit(c)" />
+            </td>
+            <td>
+              <div class="editable-cell" [hidden]="editId === c.key" (click)="startEdit(c.key)">
+                {{ c.feeCode }}
+              </div>
+              <input [hidden]="editId !== c.key" type="text" nz-input [(ngModel)]="c.feeCode" (blur)="stopEdit(c)" />
+            </td>
+            <td>
+              <div class="editable-cell" [hidden]="editId === c.key" (click)="startEdit(c.key)">
+                {{ c.amount }}
+              </div>
+              <input [hidden]="editId !== c.key" type="number" nz-input [(ngModel)]="c.amount" (blur)="stopEdit(c)" />
+            </td>
+            <td>
+              <a nz-popconfirm nzPopconfirmTitle="確定刪除？" (nzOnConfirm)="deleteRow(c.key)">刪除</a>
+            </td>
+          </tr>
+        }
       </tbody>
     </nz-table>
   `,
-  styles: [``]
+  styles: [
+    `
+      .editable-cell {
+        position: relative;
+        padding: 5px 12px;
+        cursor: pointer;
+      }
+      .editable-row:hover .editable-cell {
+        border: 1px solid #d9d9d9;
+        border-radius: 4px;
+        padding: 4px 11px;
+      }
+    `
+  ]
 })
 export class ContractListComponent implements OnInit {
   contracts: Contract[] = [];
-  clients: string[] = [];
   editId: string | null = null;
-  editField: keyof Contract | null = null;
-  editValue: any = '';
+  clients: string[] = [];
 
   constructor(
     private contractService: ContractService,
-    private modal: NzModalService,
     private hubCrud: HubCrudService
   ) {}
+
   ngOnInit() {
     this.refresh();
     this.hubCrud.getClientsSettings().then(settings => {
       this.clients = settings?.list || [];
     });
   }
+
   refresh() {
     this.contractService.list().subscribe(c => (this.contracts = c));
   }
-  async add() {
-    // 取得預設業主與下一個合約序號
+
+  async addRow() {
     const [defaultClient, contractSerial] = await Promise.all([this.hubCrud.getDefaultClient(), this.hubCrud.getNextContractSerial()]);
     const newContract: Contract = {
       contractSerial,
@@ -136,33 +119,31 @@ export class ContractListComponent implements OnInit {
       amount: 0
     };
     const id = await this.contractService.add(newContract);
-    // 取得新資料後自動進入編輯
+    this.contracts = [...this.contracts, { ...newContract, key: id }];
     setTimeout(() => {
-      this.startEdit(id, 'contractName');
+      this.startEdit(id);
     }, 200);
   }
-  startEdit(id: string | undefined, field: keyof Contract) {
+
+  startEdit(id: string | undefined) {
     if (!id) return;
     this.editId = id;
-    this.editField = field;
-    const row = this.contracts.find(c => c.key === id);
-    this.editValue = row ? row[field] : '';
     setTimeout(() => {
       const input = document.querySelector('input[nz-input]:not([hidden])');
       if (input) (input as HTMLInputElement).focus();
     }, 50);
   }
-  async stopEdit(data: Contract, field: keyof Contract) {
-    if (data.key && this.editValue !== data[field]) {
-      const updated = { ...data, [field]: this.editValue };
-      this.contracts = this.contracts.map(c => (c.key === data.key ? updated : c));
-      await this.contractService.update(data.key, { [field]: this.editValue });
+
+  async stopEdit(c: Contract) {
+    if (c.key) {
+      await this.contractService.update(c.key, c);
     }
     this.editId = null;
-    this.editField = null;
-    this.editValue = '';
   }
-  remove(c: Contract) {
-    if (c.key) this.contractService.delete(c.key).then(() => this.refresh());
+
+  async deleteRow(id: string | undefined) {
+    if (!id) return;
+    await this.contractService.delete(id);
+    this.contracts = this.contracts.filter(d => d.key !== id);
   }
 }
