2c8fd1616895c642c97f2b9ca987b4eeb83e4445
ac484 <ac484@users.noreply.github.com>
2025-07-24 10:05:51 +0000

    Automated deployment: Thu Jul 24 10:05:51 UTC 2025 24498129d207b748eb703727350eb527940e7e95

---
+++ chunk-AOHTBU2I.js
@@ -0,0 +1,1 @@
+import"./chunk-57BLH6QZ.js";var o=[{path:"",loadComponent:()=>import("./chunk-EXQNGTAQ.js").then(t=>t.RoleListComponent),data:{title:"\u89D2\u8272\u5217\u8868"}}];export{o as routes};
