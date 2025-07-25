dcf2cb684aad5112455e06132dd63866cf8da7a2
ac484 <ac484@users.noreply.github.com>
2025-07-25 20:51:23 +0000

    Automated deployment: Fri Jul 25 20:51:23 UTC 2025 10a28a59fbf83440edd1f6305e5f1c3bd2143e78

---
+++ chunk-AVVKCIZM.js
@@ -0,0 +1,1 @@
+import"./chunk-57BLH6QZ.js";var o=[{path:"",loadComponent:()=>import("./chunk-566TJMRX.js").then(t=>t.DeptListComponent),data:{title:"\u90E8\u9580\u5217\u8868"}}];export{o as routes};
---
+++ chunk-VKAS2T6V.js
@@ -0,0 +1,1 @@
+import"./chunk-57BLH6QZ.js";var o=[{path:"",loadComponent:()=>import("./chunk-JM7XJGX7.js").then(t=>t.RoleListComponent),data:{title:"\u89D2\u8272\u5217\u8868"}}];export{o as routes};
